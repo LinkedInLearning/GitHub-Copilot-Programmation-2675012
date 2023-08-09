ca383fa83c8c8b265b0522dbc311160ab6ce4c02
Sandy Ludosky <sludosky@xebia.fr>
2023-08-09 14:12:18 +0200

    03_04_d

---
+++ exercise-files/03/03_04/starter/index.js
@@ -0,0 +1,38 @@
+/**
+ * import express
+ * create a server with node and express
+ * create an instance of express
+ * create a array of blog items objects with title and content
+ * create a port 8080
+ * create a route for the home page
+ * create a callback function and listen to the port
+ **/
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+const port = 8080;
+
+const blogItems = [];
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/posts", (req, res) => {
+  res.status(200).json(blogItems);
+});
+
+app.post("/posts/insert", (req, res) => {
+  blogItems.push({
+    id: Number(new Date()),
+    ...req.body,
+  });
+  res.status(200).json(blogItems);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
