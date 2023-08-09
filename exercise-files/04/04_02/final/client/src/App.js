812e47caa35a6ed4d9bda7eb1c5278ae22db8155
Sandy Ludosky <sludosky@xebia.fr>
2023-08-09 17:01:01 +0200

    04_02_f

--- exercise-files/04/04_02/final/client/src/App.js
+++ exercise-files/04/04_02/final/client/src/App.js
@@ -1,18 +1,56 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-function BlogPosts() {
+function App() {
   const [posts, setPosts] = useState([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newPost = { title, content };
+    fetch("http://localhost:8080/posts/insert", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newPost),
+    })
+      .then((response) => response.json())
+      .then(setPosts)
+      .catch((error) => console.error(error));
+  };
 
   useEffect(() => {
     fetch("http://localhost:8080/posts")
       .then((res) => res.json())
-      .then((data) => setPosts(data));
+      .then(setPosts);
   }, []);
 
   return (
     <div>
       <h1>Blog Posts</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Title:
+          <input
+            type="text"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />{" "}
+          <br />
+        </label>
+        <br />
+        <label>
+          Content:
+          <textarea
+            value={content}
+            onChange={(event) => setContent(event.target.value)}
+          />{" "}
+          <br />
+        </label>{" "}
+        <br />
+        <button type="submit">Submit</button>
+      </form>
       <ul>
         {posts.map((post) => (
           <li key={post.id}>
@@ -21,14 +59,6 @@ function BlogPosts() {
           </li>
         ))}
       </ul>
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <div className="App">
-      <BlogPosts />
     </div>
   );
 }
