c109eb567f3d8f192da5757ad32fb31eb4a4c355
Sandy Ludosky <sludosky@xebia.fr>
2023-08-09 13:43:56 +0200

    03_02e

--- exercise-files/03/03_02/final/index.js
+++ exercise-files/03/03_02/final/index.js
@@ -3,10 +3,24 @@
  * create a server with node and express
  * create an instance of express
  * create a array of blog items objects with title and content
- * create a port
+ * create a port 8080
  * create a route for the home page
  * create a callback function and listen to the port
  **/
 
 const express = require("express");
 const cors = require("cors");
+
+const app = express();
+app.use(cors());
+const port = 8080;
+
+const blogItems = [];
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
