0f401bda5d0104a5104d462f15fa851f7a3a6dac
Sandy Ludosky <sludosky@xebia.fr>
2023-08-09 12:42:37 +0200

    02

---
+++ exercise-files/03/03_02/starter copy/index.js
@@ -0,0 +1,12 @@
+/**
+ * create a server with node and express
+ * import express
+ * create an instance of express
+ * create a array of blog items objects with title and content
+ * create a port
+ * create a route for the home page
+ * create a callback function and listen to the port
+ **/
+
+const express = require("express");
+const cors = require("cors");
